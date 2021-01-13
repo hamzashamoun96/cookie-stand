4b41197913289d69b7a00cd013e012843238d4a9
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-01-13 17:53:55 +0200

    FINAL

--- js/app.js
+++ js/app.js
@@ -1,25 +1,36 @@
 'use strict';
 
 var workhours = ['6:00 am ', '7:00 am ', '8:00 am ', '9:00 am ', '10:00 am ', '11:00 am ', '12:00 pm ', '1:00 pm ', '2:00 pm ', '3:00 pm ', '4:00 pm ', '5:00 pm ', '6:00 pm ', '7:00 pm '];
+
+// Create table , Create tr and Create th blank.
+
 var Main = document.getElementById('main');
-var tabletag = document.createElement('table');  // Create table.
-var trtag = document.createElement('tr');        // Create tr.
-var thtag = document.createElement('th');        // Create tr blank.
+var tabletag = document.createElement('table');
+tabletag.setAttribute("id","table1")  
+var trtag = document.createElement('tr');        
+var thtag = document.createElement('th');      
 var tdtag;
 Main.appendChild(tabletag);
 tabletag.appendChild(trtag);
 trtag.appendChild(thtag);
 
+// Creat th for workhours.
+
 for (var x = 0; x < workhours.length; x++) {
-    thtag = document.createElement('th');     // Creat th for workhours.
+    thtag = document.createElement('th');     
     trtag.appendChild(thtag);
     thtag.textContent = workhours[x];
 }
-thtag = document.createElement('th');         // Creat th for daily location total.
+
+// Creat th for daily location total.
+
+thtag = document.createElement('th');        
 trtag.appendChild(thtag);
 thtag.textContent = ' Daily Location Total';
 
-var StoreArray = [];
+// Creating a constructor.
+
+var StoreArray = [];                      
 function Branches(location, minnumcusph, maxnumcusph, avgcookiepcus) {
     this.location = location;
     this.minnumcusph = minnumcusph;
@@ -30,6 +41,9 @@ function Branches(location, minnumcusph, maxnumcusph, avgcookiepcus) {
     this.numcookiepurch = [];
     StoreArray.push(this);
 }
+
+// Prototype Function To Calculate Custumer Per Hour.
+
 Branches.prototype.calcusperhour = function () {
     var randomcusnum;
     for (var i = 0; i < workhours.length; i++) {
@@ -37,6 +51,9 @@ Branches.prototype.calcusperhour = function () {
         this.cusperhour.push(randomcusnum);
     }
 }
+
+// Prototype Function To Calculate Cookies Purshaced.
+
 Branches.prototype.calcnumcookiepurch = function () {
     var cookiespurchased;
     for (var i = 0; i < workhours.length; i++) {
@@ -45,6 +62,9 @@ Branches.prototype.calcnumcookiepurch = function () {
         this.totalcookies = this.totalcookies + this.numcookiepurch[i];
     }
 }
+
+// Prototype Function To Render The Results In A Table.
+
 Branches.prototype.TabelResults = function () {
     var tdtag;
     trtag = document.createElement('tr');
@@ -60,7 +80,35 @@ Branches.prototype.TabelResults = function () {
     tdtag = document.createElement('td');
     trtag.appendChild(tdtag);
     tdtag.textContent = this.totalcookies;
+
 }
+
+// Render And Calculate The Total Per Hour & Total Of Totals.
+
+function TotalAndTolalOfTotalsRow(){
+    trtag = document.createElement('tr');
+    tabletag.appendChild(trtag);
+    tdtag = document.createElement('td');
+    trtag.appendChild(tdtag);
+    tdtag.textContent = 'Total';
+    var sumcookieperhour;
+    var Totaloftotals = 0;
+    for (var y = 0; y < workhours.length; y++) {
+        sumcookieperhour = 0;
+        for (var z = 0; z < StoreArray.length; z++) {
+            sumcookieperhour = StoreArray[z].numcookiepurch[y] + sumcookieperhour
+        }
+        Totaloftotals = Totaloftotals + sumcookieperhour;
+        tdtag = document.createElement('td');
+        trtag.appendChild(tdtag);
+        tdtag.textContent = sumcookieperhour;
+    }
+    tdtag = document.createElement('td');
+    trtag.appendChild(tdtag);
+    tdtag.textContent = Totaloftotals;
+    }
+    
+// Ceating New Objects.
 
 var Seattle = new Branches('Seattle', 23, 65, 6.3)
 var Tokyo = new Branches('Tokyo', 3, 24, 1.2)
@@ -68,31 +116,32 @@ var Dubai = new Branches('Dubai', 11, 38, 3.7)
 var Paris = new Branches('Paris', 20, 38, 2.3)
 var Lima = new Branches('Lima', 2, 16, 4.6)
 
+// Calling Objects Methods.
+ 
 for(var i = 0;i<StoreArray.length;i++){
     StoreArray[i].calcusperhour();
     StoreArray[i].calcnumcookiepurch();
     StoreArray[i].TabelResults();
 }
+TotalAndTolalOfTotalsRow();
 
-console.log('Arrayofobject', StoreArray);
-trtag = document.createElement('tr');
-tabletag.appendChild(trtag);
-tdtag = document.createElement('td');
-trtag.appendChild(tdtag);
-tdtag.textContent = 'Total';
 
-var sumcookieperhour;
-var Totaloftotals = 0;
-for (var y = 0; y < workhours.length; y++) {
-    sumcookieperhour = 0;
-    for (var z = 0; z < StoreArray.length; z++) {
-        sumcookieperhour = StoreArray[z].numcookiepurch[y] + sumcookieperhour
-    }
-    Totaloftotals = Totaloftotals + sumcookieperhour;
-    tdtag = document.createElement('td');
-    trtag.appendChild(tdtag);
-    tdtag.textContent = sumcookieperhour;
+// User Input A New Object.
+
+var forminput = document.getElementById('forminput');
+forminput.addEventListener('submit', newBranch);
+
+function newBranch(event){
+    
+    event.preventDefault();
+    var location = event.target.location.value;
+    var minimumcustomer = event.target.minimum.value;
+    var maximumcustomer = event.target.maximum.value;
+    var averagecustomer = event.target.averagecookie.value;
+    var newlocation = new Branches (location,minimumcustomer,maximumcustomer,averagecustomer);
+    newlocation.calcusperhour();
+    newlocation.calcnumcookiepurch();
+    newlocation.TabelResults();
+    document.getElementById('table1').deleteRow(StoreArray.length);
+    TotalAndTolalOfTotalsRow();
 }
-tdtag = document.createElement('td');
-trtag.appendChild(tdtag);
-tdtag.textContent = Totaloftotals;
